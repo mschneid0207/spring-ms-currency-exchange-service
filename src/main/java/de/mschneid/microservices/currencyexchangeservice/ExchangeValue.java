217792abe1d5911c43b1f018aefae947b5c4ed3e
Markus Schneider <markus.schneider0207@gmail.com>
2018-09-05 22:43:41 +0200

    initial commit

---
+++ src/main/java/de/mschneid/microservices/currencyexchangeservice/ExchangeValue.java
@@ -0,0 +1,20 @@
+package de.mschneid.microservices.currencyexchangeservice;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class ExchangeValue {
+
+    private Long id;
+    private String from;
+    private String to;
+    private BigDecimal conversionMultiple;
+}
