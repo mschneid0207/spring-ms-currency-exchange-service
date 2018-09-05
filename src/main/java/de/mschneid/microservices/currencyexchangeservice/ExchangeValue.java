30f6a3a6057c16c15c3f52088d2e94e56407feb9
Markus Schneider <markus.schneider0207@gmail.com>
2018-09-05 23:18:16 +0200

    configure jpa and initialize data

--- src/main/java/de/mschneid/microservices/currencyexchangeservice/ExchangeValue.java
+++ src/main/java/de/mschneid/microservices/currencyexchangeservice/ExchangeValue.java
@@ -5,16 +5,26 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Transient;
 import java.math.BigDecimal;
 
 @Data
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
+@Entity
 public class ExchangeValue {
 
+    @Id
     private Long id;
+    @Column(name = "currency_from")
     private String from;
+    @Column(name = "currency_to")
     private String to;
     private BigDecimal conversionMultiple;
+    @Transient
+    private int port;
 }
---
+++ src/main/resources/data.sql
@@ -0,0 +1,2 @@
+insert into exchange_value(id, currency_from, currency_to, conversion_multiple)
+values  (10001, 'USD', 'EURO', 65);
